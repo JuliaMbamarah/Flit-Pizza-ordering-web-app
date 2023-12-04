7cb369294f5866d92c4718c4ab2ee72177f732df
Julia Mbamarah <104596615+JuliaMbamarah@users.noreply.github.com>
2023-12-04 09:30:13 +0100

    Update client.tsx

--- src/components/client.tsx
+++ src/components/client.tsx
@@ -1,21 +1,25 @@
 "use client";
-import PropTypes from 'prop-types'; // Import PropTypes
-import { useState } from "react";
+import PropTypes from 'prop-types';
+import { FC } from 'react'; // Import FC (Functional Component type) from React
 import "../app/globals.css";
 
-function Client({ name, img }) {
+interface ClientProps {
+  name: string;
+  img: string;
+}
+
+const Client: FC<ClientProps> = ({ name, img }) => {
   return (
     <div className="client text-center">
-      <img className="rounded-circle mb-2" src={img} alt={`${name} Image`} /> {/* Added alt text for accessibility */}
+      <img className="rounded-circle mb-2" src={img} alt={`${name} Image`} />
       <h4 className="fs-6 fw-bold">{name}</h4>
       <p className="mt-2 mb-2 text-center">
         Lorem ipsum is simply dummy text of the print book. It has survived not
-        only five centuries, but also the leap.
+        only five centuries but also the leap.
       </p>
     </div>
   );
 }
-
 
 Client.propTypes = {
   name: PropTypes.string.isRequired,
@@ -23,4 +27,3 @@ Client.propTypes = {
 };
 
 export default Client;
-        
