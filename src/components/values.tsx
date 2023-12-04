e9f82e0f15adeb5d33be75601036331534b0ff1d
Julia Mbamarah <104596615+JuliaMbamarah@users.noreply.github.com>
2023-12-04 10:38:36 +0100

    Update values.tsx

--- src/components/values.tsx
+++ src/components/values.tsx
@@ -1,17 +1,30 @@
 "use client";
-import { useState } from "react";
-import React from "react";
+import React, { FC } from "react";
+import PropTypes from 'prop-types';
 import "../app/globals.css";
 import Button from "./button";
 
-function Values({ logo, description, text }) {
+interface ValuesProps {
+  logo: string;
+  description: string;
+  text: string;
+}
+
+const Values: FC<ValuesProps> = ({ logo, description, text }) => {
   return (
     <div className="value m-2">
-      <img width="40px" className="mb-2" src={logo} />
+      <img width="40px" className="mb-2" src={logo} alt={`${description} Logo`} />
       <h6 className="fw-bold fs-6">{description}</h6>
       <p className="mt-2 mb-2">{text}</p>
     </div>
   );
 }
 
+Values.propTypes = {
+  logo: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
 export default Values;
+      
